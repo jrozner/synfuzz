cef2476d0709c5b0376b5cde24cc618f25cc6516
Joe Rozner <joe@deadbytes.net>
2018-11-30 00:01:34 -0800

    Add GenerateIter

--- synfuzz/src/lib.rs
+++ synfuzz/src/lib.rs
@@ -18,6 +18,20 @@ pub trait Generator: ::std::fmt::Debug {
     /// Generate a value from the specific implementation of the Generator
     fn generate(&self) -> Vec<u8>;
 
+    fn generate_lazy<I: Iterator>(&self) -> GenerateIter<I>;
+
     /// Generate a value of the negation of the specified Generator
     fn negate(&self) -> Vec<u8>;
+}
+
+struct GenerateIter<I: Iterator> {
+    iter: I,
+}
+
+impl<I: Iterator> Iterator for GenerateIter<I> {
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<I::Item> {
+        None
+    }
 }
